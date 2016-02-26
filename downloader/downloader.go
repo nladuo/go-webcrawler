987b347159b55b0a7d3b289e42ab0f1d78087b4b
nladuo <kalen25115@gmail.com>
2016-02-26 21:36:58 +0800

    modified:   downloader/downloader.go

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	//default time out when downloading with proxy
-	proxyTimeOut time.Duration = 0 * time.Second
+	proxyTimeOut time.Duration = 50 * time.Second
 )
 
 type Downloader interface {
@@ -35,10 +35,7 @@ func dowloadWithProxy(url string, proxy *model.Proxy) (*http.Response, error) {
 			Proxy: http.ProxyURL(proxyUrl),
 		},
 	}
-	if proxyTimeOut != 0*time.Second {
-		client.Timeout = proxyTimeOut
-	}
-
+	client.Timeout = proxyTimeOut
 	return client.Do(request)
 }
 
